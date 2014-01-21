119b91e3d3f9f935f8e08a790d9a143a27bd3be7
hunteriam <hhstuy@gmail.com>
2014-01-20 21:27:16 -0500

    merge

--- myui2.c
+++ myui2.c
@@ -14,7 +14,7 @@ Area SCREEN = {1,120,1,55,"",TRUE};
 Area newSubjectArea = {15,120,44,44,"",TRUE};
 Area newBodyArea = {12,120,45,47,"",TRUE};
 
-int colorScheme = R_COLOR_SCHEME_SELECTED;
+struct winsize window;
 
 struct TemplateString TS[] = {
 	{1,1,XT_CH_CYAN,"dbname | Max Cards cards | 					FutureDiary				(c) Hunter Herman & Tian Ci Lin"},
@@ -58,6 +58,12 @@ void debug(struct RecordList *buffer) {
 // ------------------------------------------------ main --------------------
 int main(void) {
 	int c;
+	
+	ioctl(STDOUT_FILENO,TIOCGWINSZ, &window);
+	
+	rArea.right = window.ws_col - rArea.left;
+	rArea.bot = window.ws_row - rArea.top;
+
 	
 	fill(subject,30,'\0');
 	fill(body,140,'\0');
